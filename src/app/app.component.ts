1a958f735dc700ea2781fe7fa843bf0b842584df
Harald Wellmann <harald.wellmann@gmx.de>
2019-09-15 14:24:42 +0200

    basic tune editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import abcjs from 'abcjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'folkies';
+    title = 'folkies';
+    rawAbc: string;
+
+    get abcText(): string {
+        return this.rawAbc;
+    }
+
+    set abcText(text: string) {
+        this.rawAbc = text;
+        console.log('new text: ' + this.rawAbc);
+        abcjs.renderAbc('notation', this.rawAbc);
+    }
 }
