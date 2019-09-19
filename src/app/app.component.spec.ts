0c97c4317b2a6a1d0e61164fc280aa11d86dd30f
Harald Wellmann <harald.wellmann@gmx.de>
2019-09-19 22:40:14 +0200

    it() -> test()

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,19 +21,19 @@ describe('AppComponent', () => {
         }).compileComponents();
     }));
 
-    it('should create the app', () => {
+    test('should create the app', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'folkies'`, () => {
+    test(`should have as title 'folkies'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app.title).toEqual('folkies');
     });
 
-    it('should render title in a h1 tag', () => {
+    test('should render title in a h1 tag', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
