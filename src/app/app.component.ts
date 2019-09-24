88e9f418ec2f227e30dd91cbca2ea1ef11a66cb9
Harald Wellmann <harald.wellmann@gmx.de>
2019-09-24 10:01:41 +0200

    use SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TuneBookLoaderService } from './tunebook-loader.service';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
     title = 'folkies';
