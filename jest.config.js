4a290b14c29f34effcbb5a854c524a5aaf509450
Harald Wellmann <harald.wellmann@northdata.de>
2022-05-26 23:55:53 +0200

    fixed jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,5 @@
-// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
-
-// config workaround for Angular 8, see https://github.com/briebug/jest-schematic/issues/12
-const tsJestPreset = require('jest-preset-angular/jest-preset').globals['ts-jest'];
-
 module.exports = {
   preset: 'jest-preset-angular',
-  roots: ['src'],
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
-  moduleNameMapper: {
-    '@app/(.*)': '<rootDir>/src/app/$1',
-    '@assets/(.*)': '<rootDir>/src/assets/$1',
-    '@core/(.*)': '<rootDir>/src/app/core/$1',
-    '@env': '<rootDir>/src/environments/environment',
-    '@src/(.*)': '<rootDir>/src/src/$1',
-    '@state/(.*)': '<rootDir>/src/app/state/$1'
-  },
-  globals: {
-    'ts-jest': {
-      ...tsJestPreset,
-      tsconfig: 'tsconfig.spec.json'
-    }
-  },
-  transformIgnorePatterns: ['node_modules/(?!(jest-test))']
+  globalSetup: 'jest-preset-angular/global-setup',
 };
