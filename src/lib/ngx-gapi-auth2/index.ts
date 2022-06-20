a2e9005a28b2d3886031c0ba9d27c538959562ee
Harald Wellmann <harald.wellmann@northdata.de>
2022-06-20 11:31:57 +0200

    deleted ngx-api-auth2

--- src/lib/ngx-gapi-auth2/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './lib/config/google-api.config';
-
-export * from './lib/models/auth';
-
-export * from './lib/services/google-api-loader.service'
-export * from './lib/services/google-auth2-loader.service'
-export * from './lib/services/google-auth.service'
-
-export * from './lib/common-util-google-oauth.module';
