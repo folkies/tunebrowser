6f2e099be6bae69167ac6507ee1c4690e2790faa
Harald Wellmann <harald.wellmann@gmx.de>
2023-04-15 22:59:08 +0200

    make sure that new tunes get practiced

--- src/app/service/practice-service.ts
+++ src/app/service/practice-service.ts
@@ -17,10 +17,10 @@ function randomized(date: Date): Date {
 /**
  * Builds a practice assignment from a repertoire.
  *
- * For new tunes, there is a list of predefined intervals after which a tune is due for practice.
+ * For new tunes, there is a list of N predefined intervals after which a tune is due for practice.
  * A new tune is due to be practiced every day for a week, then every two days for three times etc.
  *
- * A tune is no longer new when is has been practiced for the predefined number of times or more.
+ * A tune is no longer new when is has been practiced more than N times.
  *
  * An older tune is due for practice when it has not been practiced for a given number of days defined
  * as maximum age on the repertoire.
@@ -29,6 +29,10 @@ function randomized(date: Date): Date {
  *
  * After computing the due date for all repertoire items, the items are sorted by (randomized) due date,
  * and the required number of items is taken from the list for the current assignment.
+ * 
+ * If the remaining sorted list contains any recent items (practiced not more than N times),
+ * the assignment starts with up to N/2 items from these recent ones and is filled up with
+ * items from the sorted due list.
  */
 @Injectable()
 export class PracticeService {
@@ -44,7 +48,16 @@ export class PracticeService {
         items.sort((left, right) => differenceInDays(left.due, right.due));
         const assignment = items.filter(item => this.isDueOnDay(item, day));
         if (assignment.length >= repertoire.numTunesPerAssignment) {
-            return assignment.slice(0, repertoire.numTunesPerAssignment);
+            const due = assignment.slice(0, repertoire.numTunesPerAssignment);
+            const remaining = assignment.slice(repertoire.numTunesPerAssignment + 1);
+            const remainingRecent = remaining.filter(item => item.timesPracticed <= INTERVALS.length);
+            if (remainingRecent.length > 0) {
+                const part1 = remainingRecent.slice(0, repertoire.numTunesPerAssignment/ 2);
+                const part2 = due.slice(0, repertoire.numTunesPerAssignment - part1.length);
+                return part1.concat(part2);
+            } else {
+                return assignment.slice(0, repertoire.numTunesPerAssignment);
+            }
         }
         const remaining = items.filter(item => !this.isDueOnDay(item, day) && !this.isRecentOnDay(item, day));
         return assignment.concat(remaining.slice(0, repertoire.numTunesPerAssignment - assignment.length));
--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,2 +1,8 @@
 import 'jest-preset-angular/setup-jest';
 
+import { configure, LogLevel } from '@log4js2/core';
+
+configure({
+  level: LogLevel.INFO,
+  virtualConsole: false
+});
