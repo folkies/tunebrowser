db2907535bf2af0daef755a5577567789a8f55f0
Harald Wellmann <harald.wellmann@northdata.de>
2026-02-08 21:29:30 +0100

    updating to Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { configure, LogLevel } from '@log4js2/core';
 import { AppModule } from './app/app.module';
@@ -13,5 +13,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
